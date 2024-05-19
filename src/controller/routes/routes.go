a13f00dee9a2312389b33f8f48e021212ccffe49
Philipp <hahmann96@gmail.com>
2024-05-19 16:53:38 -0300

    feat. created the routes

---
+++ src/controller/routes/routes.go
@@ -0,0 +1,16 @@
+package routes
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/philipphahmann/crud-go/src/controller"
+)
+
+func Init(r *gin.RouterGroup) {
+
+	r.GET("/getUserById/:userId", controller.FindUserByID)
+	r.GET("/getUserByEmail/:userEmail", controller.FindUserByEmail)
+	r.POST("/createUser", controller.CreateUser)
+	r.PUT("/updateUser/:userId", controller.UpdateUser)
+	r.DELETE("/deleteUser/:userId", controller.DeleteUser)
+
+}
